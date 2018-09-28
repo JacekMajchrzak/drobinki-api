a43547d30c8f3918612efb35864feae2de05d8d5
Jacek Majchrzak <jacek.majchrzak@contractors.roche.com>
2018-09-28 14:55:51 +0200

    first commit

---
+++ src/main/java/pl/drobinki/drobinkiapi/DrobinkiApiApplication.java
@@ -0,0 +1,12 @@
+package pl.drobinki.drobinkiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrobinkiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrobinkiApiApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/drobinki/drobinkiapi/DrobinkiApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.drobinki.drobinkiapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrobinkiApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
